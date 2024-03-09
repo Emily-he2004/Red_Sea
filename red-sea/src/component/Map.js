3cd857520a4ae5ceb71454e4e142106d6698c5ec
ksskyy <149976697+ksskyy@users.noreply.github.com>
2024-03-09 13:36:57 -0800

    install mapbox-gl

--- red-sea/src/component/Map.js
+++ red-sea/src/component/Map.js
@@ -1,4 +1,15 @@
+import mapboxgl from "mapbox-gl";
+import React, { useEffect, useState } from "react";
+
 export default function Map() {
+  const [map, setMap] = useState(null);
+  const [camps, setCamps] = useState([]);
+  useEffect(() => {
+    fetch("/locations")
+      .then((res) => res.json())
+      .then((camps) => setCamps(camps));
+  }, []);
+
   return (
     <div className="Map">
       <h2>Map</h2>
