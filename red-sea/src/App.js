d542f676b4a608a98582d4651c8014d1265af2f9
ksskyy <149976697+ksskyy@users.noreply.github.com>
2024-03-09 12:56:01 -0800

    create basic component

--- red-sea/src/App.js
+++ red-sea/src/App.js
@@ -2,11 +2,13 @@ import logo from "./logo.svg";
 import "./App.css";
 import Header from "./component/Header";
 import Footer from "./component/Footer";
+import Map from "./component/Map";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Map />
       <Footer />
     </div>
   );
