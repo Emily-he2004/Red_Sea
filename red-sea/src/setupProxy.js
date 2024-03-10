209ac7f62a46a9288915507087671779e15cdec2
Shana Nursoo <153279125+reinesana@users.noreply.github.com>
2024-03-09 16:07:52 -0800

    Update setupProxy.js

--- red-sea/src/setupProxy.js
+++ red-sea/src/setupProxy.js
@@ -1,26 +1,26 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = function (app) {
+module.exports = function(app) {
   app.use(
-    "/mapbox-token",
+    '/mapbox-token',
     createProxyMiddleware({
-      target: "http://localhost:8080",
+      target: 'http://localhost:8000',
       changeOrigin: true,
     })
   );
 
   app.use(
-    "/safest-route",
+    '/safest-route',
     createProxyMiddleware({
-      target: "http://localhost:8080",
+      target: 'http://localhost:8000',
       changeOrigin: true,
     })
   );
-
+  
   app.use(
-    "/locations",
+    '/locations',
     createProxyMiddleware({
-      target: "http://localhost:8080",
+      target: 'http://localhost:8000',
       changeOrigin: true,
     })
   );
